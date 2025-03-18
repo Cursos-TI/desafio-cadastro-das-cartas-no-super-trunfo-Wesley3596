3c53771cf2e591ae7f68dcc62f2d59cfce4f2fd2
Wesley3596 <wesleysilva.03@hotmail.com>
2025-03-18 19:41:29 +0000

    ATT Cartas

---
+++ Cadastro de cartas.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("INFORMACOES DA CARTA 01 \n");
+
+    char Estado;
+    
+    printf("Insira o Estado:\n ");
+    scanf("%c", &Estado);
+    printf("Estado: %c\n", Estado);
+    
+    char Codigo[4];
+
+    printf("Insira o Codigo:\n ");
+    scanf("%s", &Codigo);
+    printf("Codigo: %s\n", Codigo);
+
+    char Cidade[10];
+
+    printf("Insira a Cidade:\n ");
+    scanf("%s", &Cidade);
+    printf("Cidade: %s\n", Cidade);
+
+    int  Populacao;
+
+    printf("Insira o numero da população:\n ");
+    scanf("%d", &Populacao);
+    printf("População: %d\n", Populacao);
+
+    float Area;
+
+    printf("Insira a Area:\n ");
+    scanf("%f", &Area);
+    printf("Area: %.2f km²\n", Area);
+
+    float PIB;
+
+    printf("Insira o PIB:\n ");
+    scanf("%f", &PIB);
+    printf("PIB: %.2f bilhoes de reais\n", PIB);
+
+    int PontosTuristicos;
+
+    printf("Insira o numero de Pontos Turisticos:\n ");
+    scanf("%d", &PontosTuristicos);
+    printf("pontosturisticos: %d \n", PontosTuristicos);
+
+
+//INFORMAÇÕES DA CARTA 02//
+
+
+    printf("INFORMACOES DA CARTA 02 \n");
+
+    char estado;
+    
+    printf("Insira o estado:\n ");
+    scanf("%c", &estado);
+    printf("Estado:%c\n", estado);
+
+
+    char codigo[4];
+
+    printf("Insira o codigo: ");
+    scanf("%s", &codigo);
+    printf("Codigo: %s\n", codigo);
+
+    char cidade[10];
+
+    printf("Insira a cidade:\n ");
+    scanf("%s", &cidade);
+    printf("Cidade: %s\n", cidade);
+
+    int  populacao;
+
+    printf("Insira o numero da populacao:\n ");
+    scanf("%d", &populacao);
+    printf("população: %d\n", populacao);
+
+    float area;
+
+    printf("Insira a area:\n ");
+    scanf("%f", &area);
+    printf("Area: %.2f km²\n", area);
+
+    float PIB_;
+
+    printf("Insira o PIB_:\n ");
+    scanf("%f", &PIB_);
+    printf("PIB: %.2f bilhoes de reais\n", PIB_);
+
+    int pontosTuristicos;
+
+    printf("Insira o numero de pontosTuristicos:\n ");
+    scanf("%d", &pontosTuristicos);
+    printf("pontosturisticos: %d\n", pontosTuristicos);
+
+
+
+
+
+    return 0;
+
+
+
+}
